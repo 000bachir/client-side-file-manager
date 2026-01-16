20cc73d55957643f56c764f0ba44dbe8ee028358
000bachir <peakdev698@gmail.com>
2026-01-16 12:06:10 +0100

    need more fixes

--- app/pages/tools/image-converter/png-jpeg.vue
+++ app/pages/tools/image-converter/png-jpeg.vue
@@ -1,3 +1,5 @@
+<!--!PNG TO JPEG-->
+
 <script setup lang="ts">
 
 import { checkingForMissingElement } from '~/utils/CheckElement';
@@ -6,11 +8,10 @@ const fileInput = ref<HTMLInputElement | null>(null);
 const convertBtn = ref<HTMLButtonElement | null>(null);
 const canvas = ref<HTMLCanvasElement | null>(null);
 const downloadLink = ref<HTMLAnchorElement | null>(null)
-
-
+const Message = ref<HTMLHeadingElement | null>(null);
 let elements = {
     fileInput, convertBtn, canvas, downloadLink
-}
+};
 
 
 fileInput.value?.addEventListener("change", onFileUpload)
@@ -31,55 +32,67 @@ function allowedFileExtensionAndMime(file: File) {
             console.error("error the file mime type is not accepted nor supported\n")
             return
         }
-        return file
+
+        if (file.size < 100) {
+            console.error("file too small for being a png file\n")
+            return false;
+        };
+
+        return file.size <= 20 * 1024 * 1024;
     } catch (error) {
         throw new Error(`error the function that check the allowed file type and mime just crashed see error : ${error}`)
     }
 }
 
-function loadImage(file : File){
-    try {
-        return new Promise((resolve , reject)=>{
-            const image =  new Image();
-            const objectUrl = URL.createObjectURL(file)
 
-            image.onload = () =>{
-                URL.revokeObjectURL(objectUrl) // clean up the temporary url
-                resolve(image)
-            }
-
-            image.onerror = () =>{
-                URL.revokeObjectURL(objectUrl)
-                reject(new Error(
-                    "failed to load the image please ensure it is a png image"
-                ))
-            }
-        })
-    }catch (error){
-        console.error(
-            `error the loadImage function crashed please see error : ${error}\n`
-        )
-    }
+function checkImageSize(){
+    const maxImageAllowed : number = 50 * 1024 *1024
+    
 }
 
 
-function convertCanvasToJpeg(){
-    let jpegToURL = canvas.value?.toDataURL('image/jpeg',0.9)
-
-    // updating the link with the jpeg
-    if(downloadLink.value){
-        downloadLink.value.href = jpegToURL
-    }
-
+function showMessageError(message: string, type: any) {
+    if (!Message.value) return;
+    Message.value.textContent = "";
+    Message.value.style = type === 'error' ? 'text-red-500' : 'text-green-500'
 }
 
+function loadPngFile(event: Event) {
+        let inputFile = event.target as HTMLInputElement
+        if (!inputFile.files) return;
+        if (!Message.value) return;
+        let file = inputFile.files[0];
+        Message.value.textContent = "";
+    
+        if (!file) {
+            showMessageError("No file selected please choose a file ", "error")
+            return
+        };
+    
+        const reader = new FileReader();
+        reader.onload = (event: Event) => {
+            let image = new Image();
+            image.src = (event.target as FileReader)!.result as string;
+            console.log(image)
+        }
+        reader.onerror = () => {
+            showMessageError("Error reading the file. Please try again.", "error");
+            return
+        }
+        reader.readAsDataURL(file);
+}
 
-function onFileUpload(event?: any) {
-    if (event.target.files.length > 0) {
-        if (convertBtn.value) convertBtn.value.disabled = false;
-    } else {
-        if (convertBtn.value) convertBtn.value.disabled = true;
-    }
+async function onFileUpload() {
+
+    const files = fileInput.value?.files; 
+    if(!files || files.length === 0) return;
+    let file = files[0];
+    if(!file) return;
+
+    if(!allowedFileExtensionAndMime(file)) return;
+
+
+
 }
 
 </script>
@@ -91,8 +104,7 @@ function onFileUpload(event?: any) {
             <UButton id="convertBtn" ref="convertBtn" color="warning" variant="soft" size="xl" disabled>Convert to JPEG
             </UButton>
             <canvas class="hidden" ref="canvas"></canvas>
-            <a id="downloadLink" ref="downloadLink" style="display: none;">Download JPEG</a>
-
+            <p id="meassage" ref="Message"></p>
         </section>
     </main>
 </template>
