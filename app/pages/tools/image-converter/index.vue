3726a4e44e29d31cf1b6a3ab23f06d88a7bf3ca8
000bachir <peakdev698@gmail.com>
2026-07-29 20:35:34 +0100

    more commit

--- app/pages/tools/image-converter/index.vue
+++ app/pages/tools/image-converter/index.vue
@@ -1,16 +1,52 @@
 <!--TODO:  RE STYLING THE PART WHERE THERE ALL THE BUTTONS THAT LEADS TO DIFFERENT FUNCTIONALITY-->
 
+<!--
+jpeg → png ----> done
+png → jpeg ----> done
+png → webp ----> done
+jpeg → webp ----> done
+webp → png
+webp → jpeg
+webp → avif
+jpeg → avif
+png → avif
+
+-->
+
 
 
 <script setup lang="ts">
 
-import { ref , onMounted } from 'vue';
+import { ref, onMounted } from 'vue';
+import JpegIcon from "@/assets/icons/ImageIcons/JpegIcon.vue"
+
+const cards = ref<HTMLElement | null>(null);
+
+onMounted(() => {
+    if (!cards.value) {
+        console.error("could not find the parent with the hashtag #cards")
+        return
+    };
+
+    cards.value.addEventListener("mousemove", (event: MouseEvent) => {
+        let cardElements = cards.value?.querySelectorAll<HTMLElement>(".card")
+        if (!cardElements) {
+            return;
+        }
+        for (const card of cardElements) {
+            const rect = card.getBoundingClientRect()
+            let x: number = event.clientX - rect.left;
+            let y: number = event.clientY - rect.top;
+
+            console.log(x);
+            console.log(y)
+
+            card.style.setProperty("--mouse-x", `${x}px`);
+            card.style.setProperty("--mouse-y", `${y}px`)
+        }
+    })
 
 
-const cards = ref(null)
-const card = ref(null)
-
-onMounted(()=>{
 })
 
 interface ImageOperations {
@@ -42,7 +78,7 @@ function formatLabel(id: string) {
 </script>
 
 <template>
-    <!--<section class="h-dvh w-full relative overflow-hidden flex items-center justify-center">
+    <!-- <section class="h-dvh w-full relative overflow-hidden flex items-center justify-center">
         <div id="button-container"
             class="h[90%] w-[90%] mx-auto relative flex items-center justify-center gap-4 flex-wrap">
             <NuxtLink v-for="operation in imageOperations" :key="operation.id"
@@ -52,33 +88,37 @@ function formatLabel(id: string) {
                 </UButton>
             </NuxtLink>
 
-            <!-- child operation renders here 
+             child operation renders here 
             <NuxtPage />
         </div>
     </section> -->
 
-    <section class="h-auto w-full relative overflow-hidden flex items-center justify-center m-0 p-0">
-        <div ref="cards" id="cards" class="flex flex-wrap gap-2 max-w-[916px] w-[calc(100% - 20px)]">
-            <div ref="card" class="card cursor-pointer flex h-[260px] flex-col relative w-[300px]">
 
-                <div class="card-content">
-                    <div class="card-image">
-                        <i class="fa-duotone fa-apartment"></i>
-                    </div>
-                    <div class="card-info-wrapper">
-                        <div class="card-info">
-                            <i class="fa-duotone fa-apartment"></i>
-                            <div class="card-info-title">
-                                <h3>Apartments</h3>
-                                <h4>Places to be apart. Wait, what?</h4>
-                            </div>
-                        </div>
-                    </div>
+    <main class="h-auto w-[95%] mx-auto relative overflow-hidden">
+        <!-- parent tag holding everything -->
+        <section class="h-auto w-full relative flex flex-wrap justify-center gap-2.5 bg-red-50 py-4 px-12">
+            <div class="container h-80 w-96  border border-gray-500 rounded-2xl bg-amber-200 grid grid-rows-3 ">
+                <div id="logo" class="row-span-1 w-full relative overflow-hidden flex items-center justify-center px-2">
+                    <JpegIcon height="5rem" width="5rem" color="#000"/>
                 </div>
+                <div class=""></div>
 
             </div>
-        </div>
-    </section>
+            <div class="container h-80 w-96  border border-gray-500 rounded-2xl bg-amber-200 "></div>
+            <div class="container h-80 w-96  border border-gray-500 rounded-2xl bg-amber-200 "></div>
+            <div class="container h-80 w-96  border border-gray-500 rounded-2xl bg-amber-200 "></div>
+            <div class="container h-80 w-96  border border-gray-500 rounded-2xl bg-amber-200 "></div>
+            <div class="container h-80 w-96  border border-gray-500 rounded-2xl bg-amber-200 "></div>
+            <div class="container h-80 w-96  border border-gray-500 rounded-2xl bg-amber-200 "></div>
+            <div class="container h-80 w-96  border border-gray-500 rounded-2xl bg-amber-200 "></div>
+            <div class="container h-80 w-96  border border-gray-500 rounded-2xl bg-amber-200 "></div>
+
+
+
+        </section>
+    </main>
+
+
 
 </template>
 
